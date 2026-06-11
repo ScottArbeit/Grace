00795b08f380efd08dcf2bcb6aaf771508d4cbbb
Scott Arbeit <scottarbeit@github.com>
2026-06-11 05:30:51 -0700

    Persist local SQLite Blake3 state

--- src/Grace.CLI.LocalStateDb.Worker/Program.fs
+++ src/Grace.CLI.LocalStateDb.Worker/Program.fs
@@ -7,7 +7,7 @@ open Grace.Types.Common
 open NodaTime
 
 module Program =
-    let private run (dbPath: string) (rootId: Guid) (rootHash: string) (iterations: int) =
+    let private run (dbPath: string) (rootId: Guid) (rootSha256Hash: string) (rootBlake3Hash: string) (iterations: int) =
         task {
             let baseTicks =
                 SystemClock
@@ -20,10 +20,28 @@ module Program =
             while index < iterations do
                 let ticks = baseTicks + int64 index
 
+                let rootDirectory =
+                    LocalDirectoryVersion.CreateWithHashes
+                        rootId
+                        Guid.Empty
+                        Guid.Empty
+                        Guid.Empty
+                        "."
+                        rootSha256Hash
+                        rootBlake3Hash
+                        (System.Collections.Generic.List<DirectoryVersionId>())
+                        (System.Collections.Generic.List<LocalFileVersion>())
+                        0L
+                        DateTime.UtcNow
+
+                let graceIndex = GraceIndex()
+                graceIndex.TryAdd(rootId, rootDirectory) |> ignore
+
                 let status =
                     { GraceStatus.Default with
+                        Index = graceIndex
                         RootDirectoryId = rootId
-                        RootDirectorySha256Hash = rootHash
+                        RootDirectorySha256Hash = rootSha256Hash
                         LastSuccessfulFileUpload = Instant.FromUnixTimeTicks(ticks)
                         LastSuccessfulDirectoryVersionUpload = Instant.FromUnixTimeTicks(ticks)
                     }
@@ -35,16 +53,17 @@ module Program =
     [<EntryPoint>]
     let main argv =
         try
-            if argv.Length < 4 then
-                Console.Error.WriteLine("Usage: <dbPath> <rootId> <rootHash> <iterations>")
+            if argv.Length < 5 then
+                Console.Error.WriteLine("Usage: <dbPath> <rootId> <rootSha256Hash> <rootBlake3Hash> <iterations>")
                 2
             else
                 let dbPath = argv[0]
                 let rootId = Guid.Parse(argv[1])
-                let rootHash = argv[2]
-                let iterations = Int32.Parse(argv[3])
+                let rootSha256Hash = argv[2]
+                let rootBlake3Hash = argv[3]
+                let iterations = Int32.Parse(argv[4])
 
-                run dbPath rootId rootHash iterations
+                run dbPath rootId rootSha256Hash rootBlake3Hash iterations
                 |> fun task -> task.GetAwaiter().GetResult()
 
                 0
