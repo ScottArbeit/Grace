a8c4028b21b6f91e06f7e0ece48b4550281b5c98
Scott Arbeit <scottarbeit@github.com>
2026-06-11 04:48:05 -0700

    Repair legacy reference hash projection

--- src/Grace.Server.Unit.Tests/Reference.Actor.Tests.fs
+++ src/Grace.Server.Unit.Tests/Reference.Actor.Tests.fs
@@ -153,3 +153,93 @@ type ReferenceActorHashValidationTests() =
             Assert.That(repairedDto.Sha256Hash, Is.EqualTo(sha256Hash))
             Assert.That(repairedDto.Blake3Hash, Is.EqualTo(blake3Hash))
         }
+
+    [<Test>]
+    member _.LegacyCreatedEventWithEmptyBlake3HydratesFromRootShaPrefix() =
+        task {
+            let referenceId = Guid.Parse("77777777-bbbb-4444-8888-777777777777")
+            let branchId = Guid.Parse("88888888-bbbb-4444-8888-888888888888")
+            let fullSha256Hash = Sha256Hash "abcdef0123456789"
+            let prefixSha256Hash = Sha256Hash "abcdef"
+            let directoryVersion = directoryVersionWithHashes fullSha256Hash blake3Hash
+
+            let legacyCreatedEvent =
+                {
+                    Event =
+                        ReferenceEventType.Created(
+                            referenceId,
+                            ownerId,
+                            organizationId,
+                            repositoryId,
+                            branchId,
+                            directoryVersionId,
+                            prefixSha256Hash,
+                            Blake3Hash String.Empty,
+                            ReferenceType.Commit,
+                            ReferenceText "legacy prefix commit",
+                            Seq.empty
+                        )
+                    Metadata =
+                        {
+                            Timestamp = getCurrentInstant ()
+                            CorrelationId = correlationId
+                            Principal = "legacy-prefix-replay-test"
+                            ClientType = None
+                            Properties = Dictionary<string, string>()
+                        }
+                }
+
+            let getDirectoryVersion _ _ _ = Task.FromResult directoryVersion
+
+            let! repairedEvent, wasRepaired = repairLegacyCreatedEventBlake3 getDirectoryVersion legacyCreatedEvent
+            Assert.That(wasRepaired, Is.True)
+
+            let repairedDto = ReferenceDto.UpdateDto repairedEvent ReferenceDto.Default
+            Assert.That(repairedDto.Sha256Hash, Is.EqualTo(fullSha256Hash))
+            Assert.That(repairedDto.Blake3Hash, Is.EqualTo(blake3Hash))
+        }
+
+    [<Test>]
+    member _.LegacyCreatedEventWithEmptyBlake3DoesNotHydrateFromNonRootOrWrongShaPrefix() =
+        task {
+            let referenceId = Guid.Parse("99999999-bbbb-4444-8888-999999999999")
+            let branchId = Guid.Parse("aaaaaaaa-bbbb-4444-8888-aaaaaaaaaaaa")
+            let fullSha256Hash = Sha256Hash "abcdef0123456789"
+
+            let createEvent storedSha256Hash =
+                {
+                    Event =
+                        ReferenceEventType.Created(
+                            referenceId,
+                            ownerId,
+                            organizationId,
+                            repositoryId,
+                            branchId,
+                            directoryVersionId,
+                            storedSha256Hash,
+                            Blake3Hash String.Empty,
+                            ReferenceType.Commit,
+                            ReferenceText "legacy mismatch commit",
+                            Seq.empty
+                        )
+                    Metadata =
+                        {
+                            Timestamp = getCurrentInstant ()
+                            CorrelationId = correlationId
+                            Principal = "legacy-mismatch-replay-test"
+                            ClientType = None
+                            Properties = Dictionary<string, string>()
+                        }
+                }
+
+            let nonRootDirectoryVersion = childDirectoryVersionWithHashes fullSha256Hash blake3Hash
+            let getNonRootDirectoryVersion _ _ _ = Task.FromResult nonRootDirectoryVersion
+            let! _, nonRootWasRepaired = repairLegacyCreatedEventBlake3 getNonRootDirectoryVersion (createEvent (Sha256Hash "abcdef"))
+
+            let rootDirectoryVersion = directoryVersionWithHashes fullSha256Hash blake3Hash
+            let getRootDirectoryVersion _ _ _ = Task.FromResult rootDirectoryVersion
+            let! _, wrongPrefixWasRepaired = repairLegacyCreatedEventBlake3 getRootDirectoryVersion (createEvent (Sha256Hash "123456"))
+
+            Assert.That(nonRootWasRepaired, Is.False)
+            Assert.That(wrongPrefixWasRepaired, Is.False)
+        }
