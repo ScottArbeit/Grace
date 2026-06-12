38226eae821bbeab219212800d0de32596d18461
Scott Arbeit <scottarbeit@github.com>
2026-06-11 17:54:19 -0700

    Wire reference BLAKE3 locators

--- src/Grace.CLI/Program.CLI.fs
+++ src/Grace.CLI/Program.CLI.fs
@@ -481,6 +481,7 @@ module GraceCommand =
                 CommandNames =
                     [
                         "branch"
+                        "reference"
                         "diff"
                         "directory-version"
                         "watch"
@@ -832,6 +833,7 @@ module GraceCommand =
         rootCommand.Subcommands.Add(Connect.Build)
         rootCommand.Subcommands.Add(Watch.Build)
         rootCommand.Subcommands.Add(Branch.Build)
+        rootCommand.Subcommands.Add(Reference.Build)
         rootCommand.Subcommands.Add(DirectoryVersion.Build)
         rootCommand.Subcommands.Add(Diff.Build)
         rootCommand.Subcommands.Add(Repository.Build)
