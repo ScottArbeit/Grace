9d6efea8ea89ab6f28207180ecab548705d2968e
Scott Arbeit <scottarbeit@github.com>
2026-06-11 21:28:56 -0700

    Fix OpenAPI hash prefix and content reference schemas

--- sdk/generated/matrix/openapi-generator/python/test/test_get_download_uri_parameters.py
+++ sdk/generated/matrix/openapi-generator/python/test/test_get_download_uri_parameters.py
@@ -44,7 +44,7 @@ class TestGetDownloadUriParameters(unittest.TestCase):
                 organization_name = '',
                 repository_id = '',
                 repository_name = '',
-                file_version = {Class=FileVersion, RelativePath=src/App.fs, Sha256Hash=805331A98813206270E35564769E8BB59EEA02AEB7B27C7D6C63E625E1857243, Blake3Hash=9A35D91B2F631BE9025DE753139B88F7B1E71385C412BC3986FF2F38F230841D, IsBinary=false, Size=128, CreatedAt=2026-06-04T18:15:00Z, BlobUri=, ContentReference={Kind=WholeFileContent}}
+                file_version = {Class=FileVersion, RelativePath=src/App.fs, Sha256Hash=805331A98813206270E35564769E8BB59EEA02AEB7B27C7D6C63E625E1857243, Blake3Hash=9A35D91B2F631BE9025DE753139B88F7B1E71385C412BC3986FF2F38F230841D, IsBinary=false, Size=128, CreatedAt=2026-06-04T18:15:00Z, BlobUri=, ContentReference={Class=FileContentReference, ReferenceType=WholeFileContent}}
             )
         else:
             return GetDownloadUriParameters(
