425cf9642b10eadada02c7de344d5defd4630f0f
Scott Arbeit <scottarbeit@github.com>
2026-06-19 15:38:12 -0700

    Implement CAS content key fanout

--- src/Grace.Types.Tests/StorageKeys.Shared.Tests.fs
+++ src/Grace.Types.Tests/StorageKeys.Shared.Tests.fs
@@ -64,14 +64,47 @@ type StorageKeysSharedTests() =
         Assert.That(StorageKeys.wholeFileContentObjectKey first, Is.Not.EqualTo(StorageKeys.wholeFileContentObjectKey second))
 
     [<Test>]
-    member _.ContentBlockObjectKeyDependsOnlyOnContentBlockAddress() =
-        let address = ContentBlockAddress "block-blake3-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    member _.ContentBlockObjectKeyUsesFourLevelDigestFanout() =
+        let address = ContentBlockAddress "ab34cd567f43aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 
         let firstKey = StorageKeys.contentBlockObjectKey address
         let secondKey = StorageKeys.contentBlockObjectKey address
 
-        Assert.That(firstKey, Is.EqualTo("cas/content-blocks/block-blake3-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
+        Assert.That(firstKey, Is.EqualTo("cas/content/ab/34/cd/56/ab34cd567f43aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
         Assert.That(secondKey, Is.EqualTo(firstKey))
+
+    [<Test>]
+    member _.ContentBlockObjectKeyNormalizesUppercaseDigest() =
+        let address = ContentBlockAddress "AB34CD567F43AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+
+        let key = StorageKeys.contentBlockObjectKey address
+
+        Assert.That(key, Is.EqualTo("cas/content/ab/34/cd/56/ab34cd567f43aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
+
+    [<Test>]
+    member _.ContentBlockObjectKeyDoesNotUseLegacyFlatOrTypedAddressShapes() =
+        let digest = "ab34cd567f43aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+        let address = ContentBlockAddress digest
+
+        let key = StorageKeys.contentBlockObjectKey address
+
+        Assert.That(key, Is.Not.EqualTo($"cas/content/{digest}"))
+        Assert.That(key, Is.Not.EqualTo($"cas/content/ab/34/{digest}"))
+        Assert.That(key, Is.Not.EqualTo($"cas/content-blocks/{digest}"))
+        Assert.That(key, Does.Not.Contain("content-blocks"))
+        Assert.That(key, Does.Not.Contain("block-blake3"))
+
+    [<TestCase("block-blake3-ab34cd567f43aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")>]
+    [<TestCase("ab34cd")>]
+    [<TestCase("ab34cd567f43aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaz")>]
+    [<TestCase("sha256-ab34cd567f43aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")>]
+    member _.ContentBlockObjectKeyRejectsMalformedOrUnsupportedAddresses(address: string) =
+        Assert.Throws<ArgumentException>(
+            Action (fun () ->
+                StorageKeys.contentBlockObjectKey (ContentBlockAddress address)
+                |> ignore)
+        )
+        |> ignore
 
     [<Test>]
     member _.ManifestObjectKeyDependsOnlyOnManifestAddress() =
